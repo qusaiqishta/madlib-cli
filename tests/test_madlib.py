6d4dd075586a095a4a946ee3a05a2797308babf8
qusaiqishta <qeshtaqusai0@gmail.com>
2021-06-12 23:24:15 +0300

    delete the skipping tests

--- tests/test_madlib.py
+++ tests/test_madlib.py
@@ -18,28 +18,3 @@ def test_merge():
     excepted=  'life is really boring,and there is no thing to do $'
 
 
-@pytest.mark.skip("pending")
-def test_parse_template():
-    actual_stripped, actual_parts = parse_template(
-        "It was a {Adjective} and {Adjective} {Noun}."
-    )
-    expected_stripped = "It was a {} and {} {}."
-    expected_parts = ("Adjective", "Adjective", "Noun")
-
-    assert actual_stripped == expected_stripped
-    assert actual_parts == expected_parts
-
-
-@pytest.mark.skip("pending")
-def test_merge():
-    actual = merge("It was a {} and {} {}.", ("dark", "stormy", "night"))
-    expected = "It was a dark and stormy night."
-    assert actual == expected
-
-
-@pytest.mark.skip("pending")
-def test_read_template_raises_exception_with_bad_path():
-
-    with pytest.raises(FileNotFoundError):
-        path = "missing.txt"
-        read_template(path)
